3567a0fadd2eaacf4d341f7c3dc29cb8c65de078
hari-at-741058543242 <harinocharla@gmail.com>
2023-01-10 17:26:57 +0530

    new code add

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Java Home App New Build- version-10!!!!</h1> \n');
+  res.send('<h1 style="color:green;">Updated ECS with latest build </h1> \n');
 });
 
 app.listen(PORT, HOST);
